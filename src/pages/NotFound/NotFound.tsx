4400451c99afe83ab8fb3c9de7f0444a5592858a
Alan Wendorff <62715512+AlanWendorff@users.noreply.github.com>
2025-01-12 23:02:20 -0300

    feat: :lipstick: Added custom NavLink button and styled notfound screen (#4)

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,27 @@
-const NotFound = () => (
-  <div>
-    <h1>END OF THE ROAD :/</h1>
+import { ROOT } from '@constants/routes';
+import { Box, Typography, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 
-    <p>404: Not found.</p>
-  </div>
+const NotFound = () => (
+  <Box
+    sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      flexDirection: 'column',
+    }}
+  >
+    <Typography variant='h2' color='primary' gutterBottom>
+      END OF THE ROAD :/
+    </Typography>
+    <Typography variant='h4' color='text.secondary'>
+      404: Not found.
+    </Typography>
+    <Button component={Link} to={ROOT} variant='contained' color='primary' sx={{ mt: 2 }}>
+      Go to Home
+    </Button>
+  </Box>
 );
 
 export default NotFound;
